a2779fde509c6501b5259bdadbe0a1d4aa41793b
Ana Bulla <ana.bulla@correo.ucu.edu.uy>
2023-09-10 19:14:21 -0300

    Subiendo nuevas modificaciones

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class ConsolePrinter
+    {
+        public static void PrintRecipe(Recipe recipe)
+        {
+            Console.WriteLine($"Receta de {recipe.FinalProduct.Description}:");
+            foreach (Step step in recipe.GetSteps())
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+        }
+    }
+}
+
+
+// creando esta clase que permita imprimir la receta, estoy usando el principio de responsabilidad única
